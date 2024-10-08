09f2d184880bcb1fccd9228520a2de0144e46e3b
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-10-08 10:10:51 +0900

    変数と配列

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int number;
-        number = 100;
-        System.out.println(number);
+        int a = 0;
+        int b = a;
+        a = 1;
+
+        System.out.println("変数 a: " + a);
+        System.out.println("変数 b: " + b);
+
+        int[] c = new int[1];
+        c[0] = 0;
+        int[] d = c;
+        c[0] =1;
+
+        System.out.println("配列 c: " + c[0]);
+        System.out.println("配列 d: " + d[0]);
+
     }
 }
