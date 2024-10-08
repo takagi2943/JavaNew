b7d9d1eada833f66f788c84bb2d4a5c93adf675d
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-10-08 11:52:03 +0900

    正規表現

--- src/App.java
+++ src/App.java
@@ -1,44 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // // 【isEmpty は値が空であるかを調べてくれるメソッド】
-        // // 値がnull
-        // String name = null;
-        
-        // // 比較演算子でnullか判定
-        // if(name == null || name.isEmpty()) {
-        //     System.out.println("必須項目が未入力です");
-        // }
+       // メタ文字について
+       // 分割する元データ
+       String str = "りんご バナナ ミカン" ;
+       System.out.println("果物の名前(分割前): " + str);
 
+       // 半角スペースで分割する
+       String[] newStr = str.split("\\s+");
 
-        // // 【equals は文字列の比較をしてくれるメソッド】
-        
-        // //1回目に入力したパスワード
-        // String pass01 = "test1234";
-
-        // //2回目に入力したパスワード
-        // String pass02 = "test1234";
-
-        // // equalsメソッドで同じかどうか判定
-        // if(pass01.equals(pass02)) {
-        //     System.out.println("入力したパスワードは同じです");
-        // } else {
-        //     System.out.println("入力したパスワードは異なります");
-        // }
-
-
-        // 【length は文字列の数を判定してくれるメソッド】
-        // 入力したパスワード
-        String pass01 = "";
-
-        // lengthメソッドで桁数を取得、パスワードが8桁かどうか判定
-        if(pass01.length() == 0) {
-            System.out.println("桁数: " + pass01.length() + "「パスワードが未入力です」");
-        }
-        else if(pass01.length() == 8) {
-            System.out.println("桁数: " + pass01.length() + "「OK！」");
-        }
-        else{
-            System.out.println("桁数: " + pass01.length() + "「パスワードは8桁で入力してください」");
-        }
+       // 繰り返し処理
+       for(int i = 0; i < newStr.length; i++) {
+        System.out.println("果物の名前(分割後): " + newStr[i]);
+       }
     }
 }
