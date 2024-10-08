1378a7b63dd0c816c3b967c9856f129bbb3ce49d
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-10-08 11:05:44 +0900

    繰り返し処理

--- src/App.java
+++ src/App.java
@@ -1,22 +1,34 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int num01 = 6;
-        int num02 = 2;
-        int result;
+        // 繰り返し実行したい処理
+        // String colour[] = {"赤","青","黄色","緑","ピンク"};
+        // // 　for文を使ったコード
+        // for (String all: colour) {
+        //     System.out.println(all);
+        // }
 
-        result = num01 + num02;
-        System.out.println("加算: " + result);
+        // // 拡張for文を使ったコード
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println(colour[i]);
+        // }
 
-        result = num01 - num02;
-        System.out.println("減算: " + result);
+        // 回数を決めずに処理を繰り返す
+        // int num = 0;
+        // // intの値が5になるまで、処理を繰り返す
+        // while(num <= 5) {
+        //     System.out.println("numの値: " + num);
 
-        result = num01 * num02;
-        System.out.println("乗算: " + result);
+        //     // メッセージを出力する毎に、numの値を1ずつ増やす
+        //     num++;
+        // }
 
-        result = num01 / num02;
-        System.out.println("除算: " + result);
+        // 繰り返し実行したい処理を少なくとも1回は必ず実行する
+        int num = 0;
+        do {
+            System.out.println("numの値: " + num);
+            num++;
 
-        result = num01 % num02;
-        System.out.println("剰算: " + result);
+            //numの値が5になるまで、処理を繰り返す
+        } while(num <= 5);
     }
 }
