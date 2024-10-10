c20f6d3ea7e9502f30f55a210df0ae4183b3e2bb
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-10-10 11:12:00 +0900

    日付の操作

--- src/App.java
+++ src/App.java
@@ -4,25 +4,27 @@ import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Dataクラス
-        Date date01 = new Date();
+        // フォーマットを指定
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-        // Calendarクラス
+        // Calendarクラスで日付データを出力
+        Calendar date01 = Calendar.getInstance();
+        System.out.println("元のデータ(現在日時)  : " + sdf.format(date01.getTime()));
+
+        // addメソッドで加算したデータを出力(現在日時から1か月後)
         Calendar date02 = Calendar.getInstance();
+        System.out.println("元のデータ(1か月後)  : " + sdf.format(date02.getTime()));
 
-        // SimpleDateFormatクラスでフォーマットを指定
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss E");
+        // addメソッドで減算したデータを出力(現在日時から1か月前)
+        Calendar date03 = Calendar.getInstance();
+        date03.add(Calendar.MONTH, -1);
+        System.out.println("元のデータ(1か月後)  : " + sdf.format(date03.getTime()));
 
-        // Dataクラスの日付をそのまま出力
-        System.out.println("Dateクラス(フォーマットなし)  : " + date01);
-        
-        // Dateクラスの日付をSimpleDateFormatクラスで指定したフォーマットで出力
-        System.out.println("Dateクラス(フォーマットあり)  : " + sdf.format(date01));
+        // setメソッドで加算したデータを出力(2020年を指定) 
+        Calendar date04 = Calendar.getInstance();
+        date04.set(Calendar.YEAR, 2020);
+        System.out.println("指定したデータ(2020年)  : " + sdf.format(date04.getTime()));
 
-        // Calendarクラスの日付をそのまま出力
-        System.out.println("Dateクラス(フォーマットなし)  : " + date02.getTime());
 
-        // Calendarクラスの日付をSimpleDateFormatクラスで指定したフォーマットで出力
-        System.out.println("Dateクラス(フォーマットあり)  : " + sdf.format(date02.getTime()));
     }
 }
